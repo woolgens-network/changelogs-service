376198ab5285e78452c917906023584d46ea08bd
ReaperMaga <reapermaga@gmail.com>
2022-01-29 05:49:36 +0100

    Added changelog model

--- src/main/java/net/woolgens/changelogs/model/Changelog.java
+++ src/main/java/net/woolgens/changelogs/model/Changelog.java
@@ -4,6 +4,8 @@ import io.quarkus.mongodb.panache.common.MongoEntity;
 import lombok.Data;
 import org.bson.codecs.pojo.annotations.BsonId;
 
+import java.util.List;
+
 /**
  * Copyright (c) Maga, All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
@@ -16,4 +18,10 @@ public class Changelog {
 
     @BsonId
     private String id;
+
+    private String title;
+    private String author;
+    private long timestamp;
+    private List<String> lines;
+
 }
